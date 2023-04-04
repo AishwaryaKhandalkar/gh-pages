41a572e773ab1d82a023e798a48520a153b8427e
Aishwarya Khandalkar <50652448+AishwaryaKhandalkar@users.noreply.github.com>
2023-04-04 13:01:12 -0700

    build

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ function About() {
         <div>
           <p className='mb-4'>
             Hello! My name is Aishwarya and I enjoy creating things that live on the internet. </p>
-          <p className='mb-4'>Passionate, result-driven and versatile frontend engineer with 3+ years of experience designing, developing and managing complex websites, web applications. Specializes in coding complex user interface across multiple viewports and responsive designing.</p>
+          <p className='mb-4'>Passionate, result-driven and versatile frontend engineer with 3+ years of experience designing, developing and managing complex websites, web applications. Specializes in coding complex user interface across multiple viewports and responsive designing. </p>
           <p className='mb-4'>
             Spirited about going the extra mile to ensure the customer is unquestionably satisfied with the product or service provided. With emphasis on scalable and well-documented code, I look forward to working collaboratively as well as spearheading independent projects.
           </p>
